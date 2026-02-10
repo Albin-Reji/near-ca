9a4f650bf56463f5e4a9a2744b2971c21e0a7008
Albin Reji <albinrejim30@gmail.com>
2026-02-10 10:45:10 +0530

    create user entity & role

---
+++ auth-service/auth-service/src/main/java/com/nearca/auth_service/auth_service/entity/Role.java
@@ -0,0 +1,6 @@
+package com.nearca.auth_service.auth_service.entity;
+
+public enum Role {
+    USER,
+    ADMIN
+}
--- auth-service/auth-service/src/main/java/com/nearca/auth_service/auth_service/entity/Users.java
+++ auth-service/auth-service/src/main/java/com/nearca/auth_service/auth_service/entity/Users.java
@@ -1,5 +1,58 @@
 package com.nearca.auth_service.auth_service.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
 public class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID userId;
+
+    @Column(nullable = false, unique = true, length = 255)
+    private String email;
+
+    @Column(name = "password", nullable = false, length = 255)
+    private String password;
+
+    @Column(name = "full_name", nullable = false, length = 255)
+    private String fullName;
+
+
+    @Column(name = "mobile_number", nullable = false, length = 10)
+    private String mobileNumber;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
+    private Role role;
+
+    @Column(name="is_active", nullable = false)
+    private boolean isActive;
+
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate(){
+        createdAt=LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate(){
+        updatedAt=LocalDateTime.now();
+    }
+
 }
